26397a4374ca531e737c089c4423ddaa171ca75d
natanSMS <natansms07@gmail.com>
2024-03-11 14:42:03 -0300

    Lista 4 - Lacos de repeticao aninhados

---
+++ Listas/Lista4/Exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Mail;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Digite uma altura de 1 a 9:");
+        int altura = int.Parse(Console.ReadLine());
+        while(altura < 1 || altura > 9)
+        {
+            System.Console.WriteLine("altura invalida, insira uma altura de 1 a 9");
+            altura = int.Parse(Console.ReadLine());
+        }
+        int coluna = 1;
+        for(int linha = 1; linha <= altura; linha++)
+        {
+            while(coluna <= linha)
+            {
+                for(int numero = 1; numero <= coluna; numero++)
+                {
+                    System.Console.Write(numero);
+                }
+                coluna++;
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
---
+++ Listas/Lista4/Exercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Tabuleiro.InicializaTabuleiro();
+        const int TAMANHO_TABULEIRO = 8;
+        for(int coluna = 0; coluna < TAMANHO_TABULEIRO; coluna++)
+        {
+            for(int linha = 0; linha < TAMANHO_TABULEIRO; linha++)
+            {
+                if(coluna == 0 || coluna == 7 || linha == 0 || linha == 7)
+                {
+                    Tabuleiro.Adicionar(linha,coluna,"s");
+                }
+            }
+        }
+        Tabuleiro.ImprimeTabuleiro();
+    }
+}
