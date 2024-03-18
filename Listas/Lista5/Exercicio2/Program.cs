85297257e32225db05dbf7cba15e7f133c2093d9
natanSMS <natansms07@gmail.com>
2024-03-18 20:37:57 -0300

    Lista 5 - Intensivao de lacos de repeticao

--- Listas/Lista5/Exercicio2/Program.cs
+++ Listas/Lista5/Exercicio2/Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        const int FATOR_MINIMO = 1;
+        const int FATOR_MINIMO = 0;
         const int FATOR_MAXIMO = 10;
 
-        for(int fator1 = FATOR_MAXIMO; fator1 >= FATOR_MINIMO; fator1 --)
+        for(int fator1 = FATOR_MAXIMO; fator1 > FATOR_MINIMO; fator1 --)
         {
             for(int fator2 = FATOR_MINIMO; fator2 <= FATOR_MAXIMO; fator2++)
             {
