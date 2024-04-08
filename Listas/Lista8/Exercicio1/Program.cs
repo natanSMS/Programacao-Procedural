49808fbb1f5fd3538672f982aa93f68a76411ffa
natanSMS <natansms07@gmail.com>
2024-04-08 20:08:35 -0300

    Lista 8 - Structs

---
+++ Listas/Lista8/Exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+struct Pizza
+{
+    public int id;
+    public string sabor;
+    public double preco;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pizza pizza1;
+
+        pizza1.id = int.Parse(Console.ReadLine());
+        pizza1.sabor = Console.ReadLine();
+        pizza1.preco = double.Parse(Console.ReadLine());
+
+        System.Console.WriteLine(pizza1.id);
+        System.Console.WriteLine(pizza1.sabor);
+        System.Console.WriteLine(pizza1.preco);
+    }
+}
---
+++ Listas/Lista8/Exercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+struct Produto
+{
+    public int id;
+    public string nome;
+    public double preco;
+    public bool disponivelEmEstoque;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int TAMANHO_VETOR = 4;
+        Produto[] produtos = new Produto[TAMANHO_VETOR];
+
+        for (int i = 0; i < produtos.Length; i++)
+        {
+            produtos[i].id = int.Parse(Console.ReadLine());
+            produtos[i].nome = Console.ReadLine();
+            produtos[i].preco = double.Parse(Console.ReadLine());
+            produtos[i].disponivelEmEstoque = bool.Parse(Console.ReadLine());
+        }
+
+        for(int i = 0; i < produtos.Length; i++)
+        {
+            if(produtos[i].disponivelEmEstoque)
+            {
+                System.Console.WriteLine(produtos[i].nome);
+                System.Console.WriteLine(produtos[i].preco);
+            }
+        }
+    }
+}
